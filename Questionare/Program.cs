4bd3ff86bd54b8bb630d04986eee085e4aa61741
Hampus Eriksson <Hampus.Eriksson@elev.ga.ntig.se>
2023-10-02 13:00:19 +0200

    First commit

---
+++ Questionare/Program.cs
@@ -0,0 +1,41 @@
+﻿
+Player p = new();
+
+Question q1 = new();
+
+q1.Text = "1+4";
+q1.alternatives = new List<string>() { "5", "3", "1" };
+q1.CorrectAnswer = 0;
+
+Question q2 = new()
+{
+    Text = "2-1",
+    alternatives = new List<string>() { "3", "5", "1" },
+    CorrectAnswer = 2,
+};
+
+int guessTry = 0;
+
+while (guessTry < 2)
+{
+    if (q1.Ask())
+    {
+        p.Score++;
+    }
+    else
+    {
+        guessTry++;
+    }
+    if (q2.Ask())
+    {
+        p.Score++;
+    }
+    else
+    {
+        guessTry++;
+    }
+    Console.WriteLine("Congratulations, you passed all questions!!");
+}
+Console.WriteLine("Sorry, but you did not pass :(");
+
+Console.ReadLine();
---
+++ Questionare/Question.cs
@@ -0,0 +1,40 @@
+﻿class Question
+{
+    public string Text;
+    public List<string> alternatives;
+    public int CorrectAnswer;
+
+    public bool Ask()
+    {
+        Console.Clear();
+        // Ställ frågan
+        Console.WriteLine(Text);
+        //Visa alternativen (foreach)
+        int i = 1;
+        string answer;
+        int guess;
+        foreach (string alternatives in alternatives)
+        {
+            Console.WriteLine($"{i}. {alternatives}");
+            i++;
+        }
+
+        // Läs in ett svar
+        Console.WriteLine("write your guess (1, 2 or 3)");
+        answer = Console.ReadLine();
+        int.TryParse(answer, out guess);
+        guess--;
+
+        // Om svaret är rätt, returnera true
+        if (guess == CorrectAnswer)
+        {
+            Console.WriteLine("Correct!");
+            return true;
+        }
+        else
+        {
+            Console.WriteLine("Wrong!");
+            return false;
+        }
+    }
+}
